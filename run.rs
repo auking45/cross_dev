2feac64bc39dc274b90d8179cdfe66441c65f767
Jinha Hwang <auking45@gmail.com>
2025-02-02 08:02:25 +0900

    script: add dependency to the rust script
    Signed-off-by: Jinha Hwang <auking45@gmail.com>

--- run.rs
+++ run.rs
@@ -1,5 +1,15 @@
 #!/usr/bin/env -S cargo +nightly -Zscript
+---cargo
+[dependencies]
+color-eyre = { version = "0.6" }
+---
 
-fn main() {
+use color_eyre::eyre::Result;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
